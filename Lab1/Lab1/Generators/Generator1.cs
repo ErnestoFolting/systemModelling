4b0b14324faa57d1f3cc30a38a6b72c65f0f39d4
ErnestoFolting <danilsmy.edu@gmail.com>
2023-09-20 13:37:01 +0300

    Add an empty project

---
+++ Lab1/Lab1/Generators/Generator1.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1.Generators
+{
+    public class Generator1 : IGenerator
+    {
+        public List<double> Generate(int numbersToGenerate)
+        {
+
+        }
+    }
+}
---
+++ Lab1/Lab1/Generators/IGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab1.Generators
+{
+    public interface IGenerator
+    {
+        public List<double> Generate(int numbersToGenerate);
+    }
+}
