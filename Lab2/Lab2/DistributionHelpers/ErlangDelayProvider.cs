fb6da8761222c6c323d3c0b6104e975f0652c0c7
ErnestoFolting <danilsmy.edu@gmail.com>
2023-10-13 01:05:45 +0300

    Add Erlang delay provider

---
+++ Lab2/Lab2/DistributionHelpers/ErlangDelayProvider.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Numerics.Distributions;
+
+namespace Lab3.DistributionHelpers
+{
+    public class ErlangDelayProvider : IDelayProvider
+    {
+        private double _timeMean;
+        private int k;
+        public ErlangDelayProvider(double timeMean, int k)
+        {
+
+            _timeMean = timeMean;
+            this.k = k;
+        }
+        public double GetDelay()
+        {
+            Erlang erlang = new Erlang(k, _timeMean);
+            return erlang.Sample();
+        }
+    }
+}
